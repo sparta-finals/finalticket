9163f058966759bc9077500f6e9f774142f4bc9e
TTPDE <bumjin0314@gmail.com>
2024-03-29 15:28:35 +0900

    init

---
+++ src/main/java/com/sparta/finalticket/FinalticketApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.finalticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalticketApplication.class, args);
+	}
+
+}
