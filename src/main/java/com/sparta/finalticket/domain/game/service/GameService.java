9dd1cbd88478893a42e049dae7868aeb16f57b27
TTPDE <bumjin0314@gmail.com>
2024-03-31 20:26:19 +0900

    refactor : 게임 예매예정,예매가능경기 전체조회 리펙토링

--- src/main/java/com/sparta/finalticket/domain/game/service/GameService.java
+++ src/main/java/com/sparta/finalticket/domain/game/service/GameService.java
@@ -4,11 +4,11 @@ import com.sparta.finalticket.domain.game.dto.GameResponseDto;
 import com.sparta.finalticket.domain.game.entity.Game;
 import com.sparta.finalticket.domain.game.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 @RequiredArgsConstructor
@@ -19,22 +19,27 @@ public class GameService {
     //예매예정경기 전체 조회
     public List<GameResponseDto> getUpcomingGame() {
         List<Game> gameList = getGames();
-        return gameList.stream()
-                .filter(game -> game.getStartDate().isBefore(LocalDateTime.now()))
-                .map(game -> new GameResponseDto(game.getName(), game.getCategory())).toList();
+        return filterGames(game -> game.getStartDate().isBefore(LocalDateTime.now()));
     }
+
     //예매가능경기 전체 조회
     public List<GameResponseDto> getAvailableGame() {
         List<Game> gameList = getGames();
-        return gameList.stream()
-                .filter(game -> game.getStartDate().isAfter(LocalDateTime.now()))
-                .map(game -> new GameResponseDto(game.getName(), game.getCategory())).toList();
+        return filterGames(game -> game.getStartDate().isAfter(LocalDateTime.now()));
     }
+
     private List<Game> getGames() {
         List<Game> gameList = gameRepository.findAll();
-        if(gameList.isEmpty()) {
+        if (gameList.isEmpty()) {
             throw new IllegalArgumentException("조회할 수 있는 게임이 없습니다.");
         }
         return gameList;
+    }
+
+    private List<GameResponseDto> filterGames(Predicate<Game> condition) {
+        return getGames().stream()
+                .filter(condition)
+                .map(game -> new GameResponseDto(game.getName(), game.getCategory()))
+                .toList();
     }
 }
