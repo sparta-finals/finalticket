9fb3f66571817e4463c589465d31d35eaae430a3
ParkDongSeob <bagd0445@gmail.com>
2024-04-02 01:34:35 +0900

    코드 수정

--- src/main/java/com/sparta/finalticket/domain/review/repository/ReviewRepositoryCustomImpl.java
+++ src/main/java/com/sparta/finalticket/domain/review/repository/ReviewRepositoryCustomImpl.java
@@ -1,9 +1,6 @@
 package com.sparta.finalticket.domain.review.repository;
 
-import com.querydsl.core.types.Projections;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.finalticket.domain.review.dto.response.ReviewResponseDto;
-import com.sparta.finalticket.domain.review.entity.QReview;
 import com.sparta.finalticket.domain.review.entity.Review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -15,52 +12,29 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ReviewRepositoryCustomImpl implements ReviewRepositoryCustom {
 
-
-    private final JPAQueryFactory queryFactory;
-    private final QReview qReview = QReview.review;
-
     @Override
     public Optional<Review> findByGameId(Long id) {
-        Review review = queryFactory
-            .selectFrom(qReview)
-            .where(qReview.game.id.eq(id))
-            .fetchOne();
-        return Optional.ofNullable(review);
+        return Optional.empty();
     }
 
     @Override
     public List<ReviewResponseDto> findAllReviews() {
-        return queryFactory
-            .select(Projections.constructor(ReviewResponseDto.class, qReview))
-            .from(qReview)
-            .fetch();
+        return null;
     }
 
     @Override
     public List<ReviewResponseDto> findReviewsByUserId(Long userId) {
-        return queryFactory
-            .select(Projections.constructor(ReviewResponseDto.class, qReview))
-            .from(qReview)
-            .where(qReview.user.id.eq(userId))
-            .fetch();
+        return null;
     }
 
     @Override
     public List<ReviewResponseDto> findReviewsByScoreGreaterThan(int score) {
-        return queryFactory
-            .select(Projections.constructor(ReviewResponseDto.class, qReview))
-            .from(qReview)
-            .where(qReview.score.gt(score))
-            .fetch();
+        return null;
     }
 
     @Override
     public List<ReviewResponseDto> findReviewsByScoreLessThan(int score) {
-        return queryFactory
-            .select(Projections.constructor(ReviewResponseDto.class, qReview))
-            .from(qReview)
-            .where(qReview.score.lt(score))
-            .fetch();
+        return null;
     }
 }
 
