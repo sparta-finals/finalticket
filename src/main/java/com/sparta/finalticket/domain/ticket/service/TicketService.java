19c8fd6b1ceb631bb95d64033346b3ace8a6f4a5
TTPDE <bumjin0314@gmail.com>
2024-04-02 13:50:12 +0900

    refactor : 티켓팅,티켓팅취소 수정

--- src/main/java/com/sparta/finalticket/domain/ticket/service/TicketService.java
+++ src/main/java/com/sparta/finalticket/domain/ticket/service/TicketService.java
@@ -4,8 +4,10 @@ import com.sparta.finalticket.domain.game.entity.Game;
 import com.sparta.finalticket.domain.game.repository.GameRepository;
 import com.sparta.finalticket.domain.seat.entity.Seat;
 import com.sparta.finalticket.domain.seat.repository.SeatRepository;
-import com.sparta.finalticket.domain.seatsetting.entity.Seatsetting;
-import com.sparta.finalticket.domain.seatsetting.repository.SeatsettingRepository;
+import com.sparta.finalticket.domain.seatsetting.repository.SeatSettingRepository;
+import com.sparta.finalticket.domain.seatsetting.entity.SeatSetting;
+import com.sparta.finalticket.domain.ticket.entity.Ticket;
+import com.sparta.finalticket.domain.ticket.repository.TicketRepository;
 import com.sparta.finalticket.domain.user.entity.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -19,34 +21,71 @@ public class TicketService {
 
     private final SeatRepository seatRepository;
 
-    private final SeatsettingRepository seatsettingRepository;
+    private final SeatSettingRepository seatsettingRepository;
+
+    private final TicketRepository ticketRepository;
 
     //티켓팅
     @Transactional
     public void createTicket(Long gameId, Long seatId, User user) {
-        //예매된 티켓 조회
-        getSeat(gameId, seatId, user.getId());
-        //존재하는 게임 검증
-        Game game = getGame(gameId);
-        //존재하는 좌석 검증
-        Seatsetting seatSetting = getSeatsetting(seatId);
 
-        Seat seat = new Seat(game, seatSetting, user);
-        seatRepository.save(seat);
+        boolean existingTicket = seatRepository.existsByUserAndGameIdAndSeatsettingIdAndState(user, gameId, seatId, false);
+        if(!existingTicket) {
+            Game game = getGame(gameId);
+            SeatSetting seatSetting = getSeatsetting(seatId);
+
+            validateSeatExist(user, game, seatId, true);
+            Seat seat = new Seat(game, seatSetting, user, true);
+            seatRepository.save(seat);
+
+            Ticket ticket = new Ticket(user, game, seat, true);
+            ticketRepository.save(ticket);
+        } else {
+            Seat seat = getSeat(gameId, seatId, user.getId(), false);
+            seat.update(true);
+
+            Ticket ticket = getTicket(seat.getId());
+            ticket.update(true);
+        }
     }
+
     //티켓팅 취소
     @Transactional
     public void deleteTicket(Long gameId, Long seatId, User user) {
-        Seat seat = getSeat(gameId, seatId, user.getId());
-        seatRepository.delete(seat);
+            Seat seat = getSeat(gameId, seatId, user.getId(), true);
+            seat.update(false);
+
+            Ticket ticket = getTicket(seat.getId());
+            ticket.update(false);
     }
+
     private Game getGame(Long gameId) {
         return gameRepository.findById(gameId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게임입니다."));
     }
-    private Seat getSeat(Long gameId, Long seatId, Long userId) {
-        return seatRepository.findSeatByGameIdAndSeatsettingIdAndUserId(gameId, seatId, userId).orElseThrow(() -> new IllegalArgumentException("이미 예매된 좌석입니다."));
+
+    private Seat getSeat(Long gameId, Long seatId, Long userId, boolean b) {
+        return seatRepository.findSeatByGameIdAndSeatsettingIdAndUserIdAndState(gameId, seatId, userId, b)
+                .orElseThrow(() -> new IllegalArgumentException("예약되지 않은 좌석 입니다."));
     }
-    private Seatsetting getSeatsetting(Long seatId) {
+
+    private SeatSetting getSeatsetting(Long seatId) {
         return seatsettingRepository.findById(seatId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 좌석입니다"));
+    }
+
+    private Ticket getTicket(Long gameId) {
+        return ticketRepository.findBySeatId(gameId)
+                .orElseThrow(() -> new IllegalArgumentException("예약되지 않은 티켓 입니다."));
+    }
+
+    private void validateTicketExist(User user, Game game, Seat seat, Boolean b) {
+        if (ticketRepository.existsByUserAndGameAndSeatAndState(user, game, seat, b)) {
+            throw new IllegalArgumentException("이미 예매된 티켓입니다.");
+        }
+    }
+
+    private void validateSeatExist(User user, Game game, Long seatId, Boolean b) {
+        if (seatRepository.existsByUserAndGameAndSeatsettingIdAndState(user, game, seatId, b)) {
+            throw new IllegalArgumentException("이미 예매된 좌석입니다.");
+        }
     }
 }
