71c7ff9bb4cbedee63bddab48bf8d63f5871f70d
jehyun <jehyun1590@naver.com>
2024-04-02 20:25:47 +0900

    :bug: import 추가,삭제

--- src/main/java/com/sparta/finalticket/domain/seatsetting/repository/SeatSettingRepository.java
+++ src/main/java/com/sparta/finalticket/domain/seatsetting/repository/SeatSettingRepository.java
@@ -1,7 +1,8 @@
 package com.sparta.finalticket.domain.seatsetting.repository;
 
+import com.sparta.finalticket.domain.seatsetting.entity.SeatSetting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SeatsettingRepository extends JpaRepository<SeatSetting, Long> {
+public interface SeatSettingRepository extends JpaRepository<SeatSetting, Long> {
 
 }
