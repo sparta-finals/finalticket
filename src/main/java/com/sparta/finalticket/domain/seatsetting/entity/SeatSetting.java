32f4b65700577de6c57a7d8576163fb9d7470dfe
LeeJaeHyung <leejh7623@gmail.com>
2024-04-02 16:12:19 +0900

    feat: Q객체 추가 commit

--- src/main/java/com/sparta/finalticket/domain/seatsetting/entity/SeatSetting.java
+++ src/main/java/com/sparta/finalticket/domain/seatsetting/entity/SeatSetting.java
@@ -1,12 +1,7 @@
 package com.sparta.finalticket.domain.seatsetting.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.sparta.finalticket.domain.seatsetting.entity.SeatTypeEnum;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
