f19fc102b4cc4e77019f4000d0a6e066e0458812
LeeJaeHyung <leejh7623@gmail.com>
2024-04-17 20:29:47 +0900

    feat: feat/ljh-user 관리자 페이지 추가

--- src/main/java/com/sparta/finalticket/domain/user/controller/HomeController.java
+++ src/main/java/com/sparta/finalticket/domain/user/controller/HomeController.java
@@ -3,15 +3,15 @@ package com.sparta.finalticket.domain.user.controller;
 import com.sparta.finalticket.domain.user.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
-	public String home(HttpServletRequest request) {
-		User user = (User) request.getAttribute("user");
-		System.out.println("user.getUsername() = " + user.getUsername());
+	public String home(HttpServletRequest request, Model model) {
+		model.addAttribute("user",(User) request.getAttribute("user"));
 		return "index";
 	}
 }
