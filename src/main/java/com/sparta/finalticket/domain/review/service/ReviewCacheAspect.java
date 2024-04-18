3da1121477004942beb1c4a2a52883037feba367
ParkDongSeob <bagd0445@gmail.com>
2024-04-18 12:18:20 +0900

    feat : ReviewCacheAspect 코드 추가

---
+++ src/main/java/com/sparta/finalticket/domain/review/service/ReviewCacheAspect.java
@@ -0,0 +1,9 @@
+package com.sparta.finalticket.domain.review.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewCacheAspect {
+}
