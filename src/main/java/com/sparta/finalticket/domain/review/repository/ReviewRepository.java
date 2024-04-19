bc51c256b940af9ea398c11eaf0b3d81f87eac86
ParkDongSeob <bagd0445@gmail.com>
2024-04-19 12:23:39 +0900

    feat : review 코드 수정

--- src/main/java/com/sparta/finalticket/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/finalticket/domain/review/repository/ReviewRepository.java
@@ -2,9 +2,11 @@ package com.sparta.finalticket.domain.review.repository;
 
 import com.sparta.finalticket.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
     Long countByGameId(Long gameId);
 
+    @Query("SELECT AVG(r.score) FROM Review r WHERE r.game.id = :gameId")
     Double calculateAverageScoreByGameId(Long gameId);
 }
