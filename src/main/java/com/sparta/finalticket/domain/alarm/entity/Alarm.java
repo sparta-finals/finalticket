2d0afc57a8eef247c03f8859232ed4ad6eca14c3
ParkDongSeob <bagd0445@gmail.com>
2024-04-21 10:50:19 +0900

    feat : alarm 코드 수정

--- src/main/java/com/sparta/finalticket/domain/alarm/entity/Alarm.java
+++ src/main/java/com/sparta/finalticket/domain/alarm/entity/Alarm.java
@@ -14,6 +14,7 @@ import org.hibernate.annotations.Where;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Table(name = "alarm", indexes = {
+	@Index(name = "idx_game_id", columnList = "game_id"),
 	@Index(name = "idx_user_id", columnList = "user_id")
 })
 @SQLDelete(sql = "UPDATE alarm SET state = true WHERE id = ?")
