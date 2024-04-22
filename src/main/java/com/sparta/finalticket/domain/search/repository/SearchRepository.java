22baaf1cc232a9a204deecdbfd6cc6cf3cfeef7a
ParkDongSeob <bagd0445@gmail.com>
2024-04-22 17:15:24 +0900

    feat : search 코드 삭제

--- src/main/java/com/sparta/finalticket/domain/search/repository/SearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.finalticket.domain.search.repository;
-
-import com.sparta.finalticket.domain.search.entity.Search;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SearchRepository extends JpaRepository<Search, Long> {
-}
