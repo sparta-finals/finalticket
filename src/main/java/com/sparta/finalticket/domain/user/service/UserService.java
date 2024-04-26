97ff0b4969f36e2890a225a5560bf86667662e92
LeeJaeHyung <69907023+LeeJaeHyung@users.noreply.github.com>
2024-04-26 13:38:26 +0900

    feat: feat/ljh-user 프론트엔드 연결 수정 및 서비스 로직 오류 수정 회원가입 부분 + 회원정보수정 front-end :  리뷰 등록, 리뷰 삭제, 경기 삭제, 회원탈퇴 추가 (#42)
    * feat: feat/ljh-user 회원가입, 유저정보 수정 검증 로직 수정
    
    DataConversionEnum 추가
    InfoController, User, UserController, UserRequestDto, UserService 수정
    
    * feat: feat/ljh-user 회원가입, 유저정보 수정 검증 로직 수정
    
    DataConversionEnum 추가
    InfoController, User, UserController, UserRequestDto, UserService 수정
    
    * feat: feat/ljh-user 회원가입, 유저정보 수정 검증 로직 수정
    
    DataConversionEnum 추가
    InfoController, User, UserController, UserRequestDto, UserService 수정
    
    * feat: dev -> feat/ljh-user 회원가입, 유저정보 수정 검증 로직 수정
    
    DataConversionEnum 추가
    InfoController, User, UserController, UserRequestDto, UserService 수정
    
    * feat: dev -> feat/ljh-user 회원가입, 유저정보 수정 검증 로직 수정
    
    DataConversionEnum 추가
    InfoController, User, UserController, UserRequestDto, UserService 수정
    
    * fix: SpringSecurity 와 redison간의 충돌,
    해결 -> SpringSecurity 의존성 제거 + PasswordEncoder구현 (Apache Commons Codec)의존성 사용하여 DigestUtils.sha256Hex()를이용하여 암호화
    
    * feat: PasswordEncoder 구현
    
    * feat: feat/ljh-user 프론트 작업 중
    
    * feat: feat/ljh-user 프론트 작업
    경기 조회, 경기 등록 , 티켓팅 , 회원정보 조회, 회원정보 수정, 회원예매 티켓 조회, 회원 등록 리뷰 조회, 회원 등록 게임 조회
    
    * feat: feat/ljh-user 프론트 작업
    회원가입 페이지 수정
    
    * fix: feat/ljh-user 회원가입 오류 수정
    
    * feat: feat/ljh-user 관리자 페이지 추가
    
    * feat: feat/ljh-user 관리자페이지 작업 중
    
    * feat: feat/ljh-user 관리자 페이지 추가
    
    * feat: dev -> feat/ljh-user
    
    * feat: feat/ljh-user 프론트 리뷰 평점 불러오기 수정
    
    * feat: feat/ljh-user 프론트 리뷰 평점 불러오기 수정
    
    * feat: feat/ljh-user 회원가입 프론트 수정
    
    * feat: feat/ljh-user 회원가입 프론트 수정
    
    * fix: 예매 티켓 조회 부분 오류 수정
    
    * fix: 예매 티켓 조회 부분 오류 수정
    
    * fix: feat/ljh-user 결제 취소,실패시 Seat의 sate가 ture라 예매가 된것으로 처리 되는 버그 수정
    
    * feat: feat/ljh-user 기존에 있던 유저 인가 방식에서 redis에서 key값으로 유저 네임을 가져오고 유저네임을 통해 db방문 후 request에 담아 user를 controller로 전달하는 과정에서 redis에 json형식으로 유저를 저장하여 바로 값을 가져오고 해당 값을 통해서 user정보를 request에 담아 전달하는 방식으로 전환
    
    * feat: feat/ljh-user 프론트엔드 연결 수정 및 서비스 로직 오류 수정 회원가입 부분 + 회원정보수정
    front-end :  리뷰 등록, 리뷰 삭제, 경기 삭제, 회원탈퇴 추가

--- src/main/java/com/sparta/finalticket/domain/user/service/UserService.java
+++ src/main/java/com/sparta/finalticket/domain/user/service/UserService.java
@@ -30,14 +30,13 @@ public class UserService {
     @Value("${token.admin.key}")
     private String admin_token;
 
-    public boolean signup(UserRequestDto requestDto) {
+    public void signup(UserRequestDto requestDto) throws IllegalArgumentException {
         boolean validated = validateUserInfo(requestDto, DataConversionEnum.SIGNUP, null);
         if (validated) {
             // 사용자 등록
             User user = new User(requestDto);
             userRepository.save(user);
         }
-        return validated;
     }
 
     public void login(LoginRequestDto requestDto, HttpServletResponse response) {
@@ -50,17 +49,29 @@ public class UserService {
     }
 
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-        sessionUtil.logout(request,response);
+        sessionUtil.logout(request, response);
     }
 
     @Transactional
-    public boolean modifyInfo(User user, UserRequestDto requestDto) {
-        boolean validated = validateUserInfo(requestDto, DataConversionEnum.INFO, user);
-        if (validated) {
+    public void modifyInfo(User user, UserRequestDto requestDto) throws IllegalArgumentException {
+        boolean validated = false;
+        if (isChange(user, requestDto)) {//변경 체크
+            validated = validateUserInfo(requestDto, DataConversionEnum.INFO, user);
+        }
+        if (validated) {//중복 검사 결과
             User modifyUser = new User(requestDto, user);
             userRepository.modifyUserInfo(modifyUser);
+        } else {
+            throw new IllegalArgumentException("입력하신 정보가 기존정보와 일치합니다.");
         }
-        return validated;
+    }
+
+    private boolean isChange(User user, UserRequestDto requestDto) {
+        boolean isEquals = user.getUsername().equals(requestDto.getUsername()) && user.getNickname()
+            .equals(requestDto.getNickname()) && user.getEmail().equals(requestDto.getEmail())
+            && user.getAddress().equals(requestDto.getAddress()) && passwordEncoder.matches(
+            requestDto.getPassword(), user.getPassword());
+        return !isEquals;
     }
 
     @Transactional
@@ -70,61 +81,50 @@ public class UserService {
     }
 
     public boolean validateUserInfo(UserRequestDto requestDto, DataConversionEnum conversionEnum,
-        User user) {
-        try{
-            requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-            // 회원 중복 확인
-            String usernamae = requestDto.getUsername();
-            if(conversionEnum.equals(DataConversionEnum.SIGNUP)||user.getUsername().equals(usernamae)){
-                Optional<User> checkUsername = userRepository.findByUsername(usernamae);
-                if (checkUsername.isPresent()) {
-                    throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
-                }
-            }else{
+        User user) throws IllegalArgumentException {
+        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        // 회원 중복 확인
+        String username = requestDto.getUsername();
+        if (conversionEnum.equals(DataConversionEnum.SIGNUP) || !user.getUsername()
+            .equals(username)) {
+            Optional<User> checkUsername = userRepository.findByUsername(username);
+            if (checkUsername.isPresent()) {
                 throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
             }
+        }
 
-
-
-            // email 중복확인
-            String email = requestDto.getEmail();
-            if(conversionEnum.equals(DataConversionEnum.SIGNUP)||!user.getEmail().equals(email)){
-                Optional<User> checkEmail = userRepository.findByEmail(email);
-                if (checkEmail.isPresent()) {
-                    throw new IllegalArgumentException("중복된 Email 입니다.");
-                }
-            }else{
+        // email 중복확인
+        String email = requestDto.getEmail();
+        if (conversionEnum.equals(DataConversionEnum.SIGNUP) || !user.getEmail().equals(email)) {
+            Optional<User> checkEmail = userRepository.findByEmail(email);
+            if (checkEmail.isPresent()) {
                 throw new IllegalArgumentException("중복된 Email 입니다.");
             }
-
-            String nickname = requestDto.getNickname();
-            if(conversionEnum.equals(DataConversionEnum.SIGNUP)||!user.getNickname().equals(nickname)){
-                Optional<User> checkNickname = userRepository.findByNickname(nickname);
-                if (checkNickname.isPresent()) {
-                    throw new IllegalArgumentException("중복된 Nickname 입니다.");
-                }
-            }else{
+        }
+        String nickname = requestDto.getNickname();
+        if (conversionEnum.equals(DataConversionEnum.SIGNUP) || !user.getNickname()
+            .equals(nickname)) {
+            Optional<User> checkNickname = userRepository.findByNickname(nickname);
+            if (checkNickname.isPresent()) {
                 throw new IllegalArgumentException("중복된 Nickname 입니다.");
             }
+        }
 
-            if (conversionEnum.equals(DataConversionEnum.SIGNUP)) {
-                // 사용자 ROLE 확인
-                if (requestDto.isAdmin()) {
-                    if (!admin_token.equals(requestDto.getAdminToken())) {
-                        throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
-                    }
-                    requestDto.setRole(UserRoleEnum.ADMIN);
-                } else {
-                    requestDto.setRole(UserRoleEnum.USER);
+        if (conversionEnum.equals(DataConversionEnum.SIGNUP)) {
+            // 사용자 ROLE 확인
+            if (requestDto.isAdmin()) {
+                if (!admin_token.equals(requestDto.getAdminToken())) {
+                    throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
                 }
+                requestDto.setRole(UserRoleEnum.ADMIN);
+            } else {
+                requestDto.setRole(UserRoleEnum.USER);
             }
-        }catch (Exception e){
-            return false;
         }
         return true;
     }
 
-    public void test(){
+    public void test() {
         User user = new User();
         user.setUsername("admin");
         user.setNickname("admin");
