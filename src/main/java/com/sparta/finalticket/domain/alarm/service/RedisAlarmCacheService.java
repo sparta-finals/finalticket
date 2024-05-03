b1e760782527bbd8261e6ae332b86b7e6e9a5745
ParkDongSeob <bagd0445@gmail.com>
2024-05-03 12:47:20 +0900

    feat : RedisAlarmCacheService 코드 개선

--- src/main/java/com/sparta/finalticket/domain/alarm/service/RedisAlarmCacheService.java
+++ src/main/java/com/sparta/finalticket/domain/alarm/service/RedisAlarmCacheService.java
@@ -2,10 +2,7 @@ package com.sparta.finalticket.domain.alarm.service;
 
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
